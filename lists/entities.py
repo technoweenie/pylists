929769efc4616bd367fe006806dd012c79cd69b4
risk :danger: olson <technoweenie@gmail.com>
2011-11-13 13:37:12 -0700

    add list entity

--- lists/entities.py
+++ lists/entities.py
@@ -1,29 +1,71 @@
 import uuid
 
-def _thread(key, **attrs):
-    """Initializes a new Thread or passes one through.
+def _list(key, **attrs):
+    """Initializes a new List or passes one through.
 
-    a_thread    = _thread("yay", title="Yay")
-    same_thread = _thread(a_thread)
-    new_thread  = _thread("new", title="New")
+        a_list = _list("foo@bar.com", name="Foo")
+        same_list = _list(a_list)
 
-    key - Either a String Thread key or a Thread.
+        new_list = _list("bar@bar.com")
 
-    Returns a Thread.
+    key = Either a String List key or a List.
+
+    Returns a List.
     """
 
     if hasattr(key, 'key'):
         return key
     else:
-        return Thread(key, **attrs)
+        return List(key, **attrs)
+
+def _thread(*args, **attrs):
+    """Initializes a new Thread or passes one through.
+
+
+        a_list = _list("foo@bar.com")
+        a_thread = _thread(a_list, "yay", title="Yay")
+
+        same_thread = _thread(a_thread)
+
+        new_thread = _thread("foo@bar.com", "new", title="New")
+        new_thread = _thread(a_list, "new", title="New")
+        new_thread = _thread("foo@bar.com/new", title="New")
+
+    lst - Either a String List key or a List.
+    key - Either a String Thread key or a Thread.
+
+    Returns a Thread.
+    """
+
+    arg_len = len(args)
+    if arg_len == 2:
+        lst, key = args
+        if hasattr(key, 'key'):
+            return key
+        else:
+            return Thread(lst, key, **attrs)
+    elif arg_len == 1:
+        list_or_thread = args[0]
+        if hasattr(list_or_thread, 'list'):
+            return list_or_thread
+        else:
+            first, sep, last = str(list_or_thread).partition("/")
+            if sep:
+                return Thread(first, last, **attrs)
+            else:
+                return Thread(list_or_thread, None, **attrs)
 
 def _msg(*args, **attrs):
     """Initializes a new Message or passes one through.
 
-    a_msg    = _msg("thread", "yay", title="Yay")
-    same_msg = _msg("thread", a_msg)
-    same_msg = _msg(a_msg)
-    new_msg  = _msg("thread", "new", title="New")
+        a_list = _list("foo@bar.com")
+        a_thread = _thread(a_list, "yay")
+        a_msg = _msg(a_thread, "yay", title="Yay")
+
+        same_msg = _msg("list/thread", a_msg)
+        same_msg = _msg(a_msg)
+
+        new_msg = _msg("list/thread", "new", title="New")
 
     thread - Either a String Thread key or a Thread.
     key    - Either a UUID or String Message key or a Message.
@@ -53,10 +95,10 @@ def _msg(*args, **attrs):
 def _uuid(value=None):
     """Builds a UUID.
 
-      new_uuid  = _uuid()
-      same_uuid = _uuid(new_uuid)
-      same_uuid = _uuid(new_uuid.bytes) # using 16-char byte string
-      same_uuid = _uuid(str(new_uuid))  # using 36-char hex string
+        new_uuid = _uuid()
+        same_uuid = _uuid(new_uuid)
+        same_uuid = _uuid(new_uuid.bytes) # using 16-char byte string
+        same_uuid = _uuid(str(new_uuid))  # using 36-char hex string
 
     value - Optional UUID or String representation of a UUID.
 
@@ -74,9 +116,16 @@ def _uuid(value=None):
     else:
         return uuid.uuid1()
 
-class Thread(object):
+class List(object):
 
     def __init__(self, key, **attrs):
+        self.name = attrs.setdefault('name', None)
+        self.key = key
+
+class Thread(object):
+
+    def __init__(self, lst, key, **attrs):
+        self.list = _list(lst)
         self.title = attrs.setdefault('title', None)
         self.key = key
 
@@ -91,6 +140,7 @@ class Message(object):
         self.title = attrs.setdefault('title', None)
         self.created_at = attrs.setdefault('created_at', None)
         self.updated_at = attrs.setdefault('updated_at', None)
+        self.list = self.thread.list
 
     def __str__(self):
         return "<Message %s title=%s>" % (self.key, self.title)
--- test/entities_test.py
+++ test/entities_test.py
@@ -2,8 +2,19 @@ from ..lists import entities
 
 from nose.tools import assert_equal, assert_not_equal
 
+def test_list():
+    a_list = entities._list('foo@bar.com', name="Foo")
+    assert_equal("foo@bar.com", a_list.key)
+    assert_equal("Foo", a_list.name)
+
+    assert_equal(a_list, entities._list(a_list))
+
+    new_list = entities._list("bar@bar.com")
+    assert_not_equal(a_list, new_list)
+
 def test_thread():
-    a_thread = entities._thread("yay", title="Yay")
+    a_thread = entities._thread("foo@bar.com", "yay", title="Yay")
+    assert_equal("foo@bar.com", a_thread.list.key)
     assert_equal("yay", a_thread.key)
     assert_equal("Yay", a_thread.title)
 
@@ -11,10 +22,23 @@ def test_thread():
 
     new_thread = entities._thread("boo", title="Boo")
     assert_not_equal(a_thread, new_thread)
+    assert_equal("boo", new_thread.list.key)
+    assert_equal(None, new_thread.key)
+
+    new_thread = entities._thread("foo@bar.com", "boo", title="Boo")
+    assert_not_equal(a_thread, new_thread)
+    assert_equal("boo", new_thread.key)
+    assert_equal("foo@bar.com", new_thread.list.key)
+
+    new_thread = entities._thread("foo@bar.com/boo", title="Boo")
+    assert_not_equal(a_thread, new_thread)
+    assert_equal("boo", new_thread.key)
+    assert_equal("foo@bar.com", new_thread.list.key)
 
 def test_message():
     uuid = entities._uuid()
-    a_msg = entities._msg("yay", uuid, title="Message")
+    a_msg = entities._msg("foo@bar.com/yay", uuid, title="Message")
+    assert_equal("foo@bar.com", a_msg.list.key)
     assert_equal("yay", a_msg.thread.key)
     assert_equal(uuid, a_msg.key)
     assert_equal("Message", a_msg.title)
@@ -27,7 +51,20 @@ def test_message():
     assert_not_equal(a_msg, thread_msg)
 
     new_msg = entities._msg("yay", entities._uuid())
+    assert_equal("yay", new_msg.thread.list.key)
     assert_not_equal(a_msg, new_msg)
+
+def test_message_with_list():
+    lst = entities._list("foo@bar.com", name="foo")
+    thread = entities._thread(lst, "bar")
+    msg = entities._msg(thread, title="New Message")
+
+    assert_equal("New Message", msg.title)
+    assert_equal("bar", msg.thread.key)
+    assert_equal("foo", msg.list.name)
+
+    lst.name = "Foo"
+    assert_equal("Foo", msg.list.name)
 
 def test_uuid():
     uuid = entities._uuid()
